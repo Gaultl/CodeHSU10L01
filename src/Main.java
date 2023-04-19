e90fc0bb2c21c5d9dd7cbb946784f87e074ebed8
gaultl <gaultl@wlhs.wlwv.k12.or.us>
2023-04-19 09:46:55 -0700

    added recursive methods for factorial, summation, countdowns, and summing arrays

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,42 @@
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(factorial(10));
+        System.out.println(SumOfNumbers(10));
+        RecursiveCountdown(10);
+        int[] array = {2, 4, 8, 11, 7};
+        System.out.println(sumArray(array));
+    }
+
+    public static void RecursiveCountdown(int n){
+        if(n == 1){
+            System.out.println(1);
+        } else {
+            System.out.println(n);
+            RecursiveCountdown(n - 1);
+        }
+
     }
 
     public static int SumOfNumbers(int n){
-        return 0;
+        if(n == 1){
+            return 1;
+        }
+        return n + SumOfNumbers(n - 1);
+    }
+
+    public static int factorial(int n){
+        if(n == 1){
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    public static int sumArray(int[] array){
+            if(array.length == 1){
+                return array[array.length - 1];
+            }
+            int[] nextArray = Arrays.copyOf(array, array.length - 1);
+            return array[array.length - 1] + sumArray(nextArray);
     }
 }
